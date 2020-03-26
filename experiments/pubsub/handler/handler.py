a0e8779bd7190363021b76c59ae9feb7f2462d34
Mihir Garimella <mihir.garimella@gmail.com>
2020-03-26 19:31:42 -0400

    Handler code for Pubsub throughput experiments

---
+++ experiments/pubsub/handler/handler.py
@@ -0,0 +1,24 @@
+import time
+
+from flask import Flask, request
+from google.cloud import pubsub
+
+
+publish_client = pubsub.PublisherClient(
+    pubsub.types.BatchSettings(max_messages=1)
+)  # no batching
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["POST"])
+def handler():
+    delay = request.get_json()["delay"]
+    response = bytes([delay])
+
+    # Simulate computation
+    time.sleep(delay)
+
+    publish_client.publish("results", response)
+    return "", 200
