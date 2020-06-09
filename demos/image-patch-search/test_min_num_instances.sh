a001cccb759aab9117df9c7b9b3979e0191d74c5
Mihir Garimella <mihir.garimella@gmail.com>
2020-06-09 18:28:03 -0400

    min-num-instnaces

--- demos/image-patch-search/test_min_num_instances.sh
+++ demos/image-patch-search/test_min_num_instances.sh
@@ -1,14 +1,14 @@
 branch=`git rev-parse --abbrev-ref HEAD`
 project=`gcloud config get-value project 2> /dev/null`
 name=spatial-search
-url=https://mihir-spatial-search-min-num-instances-g6rwrca4fq-uc.a.run.app
+url=https://mihir-spatial-search-min-num-instances-test-g6rwrca4fq-uc.a.run.app
 
 export KNN_START_TIME=`python3 -c 'import time;print(time.time())'`
-gcloud alpha run deploy mihir-$name-$branch --image gcr.io/$project/mihir-$name-$branch --platform managed --concurrency 1 --cpu 1 --max-instances 1000 --memory 2Gi --timeout 900 --region us-central1 --allow-unauthenticated --min-instances=1000
+gcloud alpha run deploy mihir-$name-$branch-test --image gcr.io/$project/mihir-$name-$branch --platform managed --concurrency 1 --cpu 1 --max-instances 1000 --memory 2Gi --timeout 900 --region us-central1 --allow-unauthenticated --min-instances=1000
 export KNN_SLEEP_START_TIME=`python3 -c 'import time;print(time.time())'`
-sleep 180
+sleep 60
 export KNN_INNER_START_TIME=`python3 -c 'import time;print(time.time())'`
 pipenv run python benchmark.py -m $url results/$1.json
 export KNN_INNER_END_TIME=`python3 -c 'import time;print(time.time())'`
-gcloud alpha run deploy mihir-$name-$branch --image gcr.io/$project/mihir-$name-$branch --platform managed --concurrency 1 --cpu 1 --max-instances 1000 --memory 2Gi --timeout 900 --region us-central1 --allow-unauthenticated --min-instances=0
+gcloud alpha run deploy mihir-$name-$branch-test --image gcr.io/$project/mihir-$name-$branch --platform managed --concurrency 1 --cpu 1 --max-instances 1000 --memory 2Gi --timeout 900 --region us-central1 --allow-unauthenticated --min-instances=0
 export KNN_END_TIME=`python3 -c 'import time;print(time.time())'`
