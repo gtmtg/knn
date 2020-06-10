cf697b265decc8ecbc986ae9028e9be1593a5ed0
Mihir Garimella <mihir.garimella@gmail.com>
2020-06-10 19:34:08 -0400

    switch to us-west

--- mappers/reset.sh
+++ mappers/reset.sh
@@ -7,7 +7,7 @@ branch=`git rev-parse --abbrev-ref HEAD`
 project=`gcloud config get-value project 2> /dev/null`
 folder=`echo $1 | sed 's:/*$::'`
 name=mihir-$folder-$branch
-region=us-central1
+region=us-west1
 
 # Deploy Cloud Run handler
 gcloud run deploy $name --image gcr.io/$project/$name --platform managed --concurrency 1 --cpu 1 --max-instances 1000 --memory 2Gi --timeout 900 --region $region --allow-unauthenticated
